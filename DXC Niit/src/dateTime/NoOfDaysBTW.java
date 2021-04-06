80133c1f99c76ec7951482a2d45a87bb1a006ccb
srini <srini@DESKTOP-9MJ728Q>
2021-04-06 15:05:43 +0530

    6/4/21 3:05PM

---
+++ DXC Niit/src/dateTime/NoOfDaysBTW.java
@@ -0,0 +1,16 @@
+package dateTime;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class NoOfDaysBTW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LocalDate dob = LocalDate.of(2000, 02, 18);
+		Period period = Period.between(dob, LocalDate.now());
+		System.out.println(period.getYears()+ "" +period.getMonths() + period.getDays());
+	}
+
+}
