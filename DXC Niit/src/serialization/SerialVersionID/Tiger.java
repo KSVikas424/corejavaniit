5f944bf05ff7502062936266cff76e92321a53d0
srini <srini@DESKTOP-9MJ728Q>
2021-04-07 16:33:15 +0530

    SerialVersion Example

---
+++ DXC Niit/src/serialization/SerialVersionID/Tiger.java
@@ -0,0 +1,11 @@
+package serialization.SerialVersionID;
+
+import java.io.Serializable;
+
+public class Tiger implements Serializable {
+	private static final long serialVersionUID = -3823502692874356450L;
+	int a = 1000;
+	int b = 2000;
+	int c = 3000;
+
+}
